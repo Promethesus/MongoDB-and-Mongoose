6bc5960ce87a3665a66c962f88f127d631ae34c7
Promethesus <JeremyAndrews707@gmail.com>
2021-05-13 03:48:53 -0700

    corrected password format for .env file to allow connection to mongoDB

--- myApp.js
+++ myApp.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
 let Person;
 
 const createAndSavePerson = (done) => {
