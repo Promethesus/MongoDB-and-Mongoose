738317c07e23e4abb0a1f51994fba600d00447e5
Promethesus <JeremyAndrews707@gmail.com>
2021-05-25 05:06:20 -0700

    added search by id function to myapp to pass FCC test

--- myApp.js
+++ myApp.js
@@ -47,7 +47,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({personId: personId}, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data)
+  })
 };
 
 const findEditThenSave = (personId, done) => {
