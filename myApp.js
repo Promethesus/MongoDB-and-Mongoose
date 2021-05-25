b52a0d55849bacd4dfdbc3e23cfde3414a98feec
Promethesus <JeremyAndrews707@gmail.com>
2021-05-25 05:08:50 -0700

    fixed find by id to correct formatting

--- myApp.js
+++ myApp.js
@@ -47,7 +47,7 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  Person.findById({personId: personId}, function (err, data) {
+  Person.findById(personId, function (err, data) {
     if (err) return console.log(err);
     done(null, data)
   })
